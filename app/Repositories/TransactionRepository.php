983e8e3f65417ae50285dc4267ae3eb5763e563a
smithheinrich254 <heinrich@identigate.co.ke>
2024-01-29 17:54:49 +0300

    Added TransactionRepository

---
+++ app/Repositories/TransactionRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Transaction;
+use App\Interfaces\TransactionRepositoryInterface;
+
+class TransactionRepository implements TransactionRepositoryInterface
+{
+    public function getAll()
+    {
+        return Transaction::all();
+    }
+
+    public function create(array $data)
+    {
+        return Transaction::create($data);
+    }
+}

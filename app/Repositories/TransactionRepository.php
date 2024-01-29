ad5e78ab2c3d6bfd2b10a59e4d5e0a2b61113f1a
smithheinrich254 <heinrich@identigate.co.ke>
2024-01-29 18:38:28 +0300

    updated account_id to uuid

--- app/Repositories/TransactionRepository.php
+++ app/Repositories/TransactionRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\Transaction;
+use Illuminate\Support\Str;
 use App\Interfaces\TransactionRepositoryInterface;
 
 class TransactionRepository implements TransactionRepositoryInterface
@@ -14,6 +15,12 @@ class TransactionRepository implements TransactionRepositoryInterface
 
     public function create(array $data)
     {
+        // Create UUID if not provided
+
+        if (!isset($data['account_id'])) {
+            $data['account_id'] = Str::uuid();
+        }
+
         return Transaction::create($data);
     }
 

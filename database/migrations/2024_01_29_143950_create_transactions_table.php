b7c2de0930085929c6cc89c5f69608e4f52d66e6
smithheinrich254 <heinrich@identigate.co.ke>
2024-01-29 17:54:01 +0300

    Added migration for transactions

---
+++ database/migrations/2024_01_29_143950_create_transactions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            // database migration for transactions table 
+            $table->id();
+            $table->string('account_id')->nullable();
+            $table->decimal('amount', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};

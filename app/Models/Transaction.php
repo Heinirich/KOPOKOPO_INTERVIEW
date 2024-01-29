908b5156137916096591069012ca95234a5407b0
smithheinrich254 <heinrich@identigate.co.ke>
2024-01-29 18:40:18 +0300

    Added Fillable to Transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'account_id', // Add 'account_id' to the fillable array
+        'amount',
+    ];
 }
